5500f0d60c55a7150b259dff3df320faa2b6b8de
NRocket <ninamrocket@gmail.com>
2020-08-09 10:54:37 -0700

    index.bundle.js 404 error resolved

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "/index.html", "index.js"];
+const FILES_TO_CACHE = ["/", "/index.html", "index.js", "/dist/index.bundle.js"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
